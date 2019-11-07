c5e3d2eb77a9bd2759c3a7189fd3ddc7dc6383cd
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-11-07 14:05:20 +0100

    Creation static file edit.js in public + addEventListener on the edit

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ db.authenticate()
   });
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   Item.findAll().then(items => {
@@ -56,7 +57,7 @@ app.get("/", (req, res) => {
       </ul>
       
     </div>
-    
+    <script src="/js/edit.js"></script>
   </body>
   </html>`);
   });
---
+++ public/js/edit.js
@@ -0,0 +1,5 @@
+document.addEventListener("click", function(elem) {
+  if (elem.target.classList.contains("edit-me")) {
+    alert("Clicquer OK sur le bouton Modifier");
+  }
+});
